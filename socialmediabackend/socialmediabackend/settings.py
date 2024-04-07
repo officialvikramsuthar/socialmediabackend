e8937bb973f23b1e8fffae92580adafcb03c997b
Vikram <vikram@trendlyne.com>
2024-04-08 00:09:00 +0530

    Added postgres

--- socialmediabackend/socialmediabackend/settings.py
+++ socialmediabackend/socialmediabackend/settings.py
@@ -75,12 +75,15 @@ WSGI_APPLICATION = "socialmediabackend.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social_media',
+        'USER': 'vikram',
+        'PASSWORD': 'vikram9890',
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
