aab32f3a83328efe94390a2caf7e6cb961e735b3
Vikram <vikram@trendlyne.com>
2024-04-08 09:44:03 +0530

    Middleware bug fix

--- socialmediabackend/socialmediabackend/settings.py
+++ socialmediabackend/socialmediabackend/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django_ratelimit.middleware.RatelimitMiddleware",
 ]
 
 ROOT_URLCONF = "socialmediabackend.urls"
@@ -78,10 +77,10 @@ WSGI_APPLICATION = "socialmediabackend.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'social_media',
+        'NAME': 'socialmedia',
         'USER': 'vikram',
         'PASSWORD': 'vikram9890',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': '5432'
     }
 }
